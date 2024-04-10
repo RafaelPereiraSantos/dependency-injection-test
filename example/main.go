a27f28bd12a2b666a4909e49a98fe4d9754153d8
RafaelPereiraSantos <rafaelps999@gmail.com>
2024-04-10 16:22:42 -0300

    feat: add nanoseconds measuremet to examples

--- example/main.go
+++ example/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	lib "github.com/RafaelPereiraSantos/injection-test"
 	"github.com/RafaelPereiraSantos/injection-test/example/interfaces"
 	"github.com/RafaelPereiraSantos/injection-test/example/services"
@@ -8,19 +11,42 @@ import (
 )
 
 func main() {
+	regularWayTime := time.Now()
+	initializeWorkersInTheRegularWay()
+	elapsedRegularWay := time.Since(regularWayTime)
+
+	dependencyInjectionTime := time.Now()
+	initializeWorkersWithDependencyInjection()
+	elapsedDependencyInjectionTime := time.Since(dependencyInjectionTime)
+
+	fmt.Println("Elapsed time regular way in nanoseconnds:", elapsedRegularWay.Nanoseconds())
+	fmt.Println("Elapsed time dependency injection way in nanoseconnds:", elapsedDependencyInjectionTime.Nanoseconds())
+}
+
+func initializeWorkersInTheRegularWay() {
 	// Regular way of creating a new instance of a fake worker, it creates each parameter separately and then pass them
 	// in the contruction function.
+
 	srv01 := services.NewServiceImplementation01()
 	srv02 := services.NewServiceImplementation02()
 	srv03 := services.NewServiceImplementation03()
 
-	regularWrk, _ := createWorker01(srv01, srv02, srv03)
-	regularWrk.DoSomething()
+	regularWrk1, _ := createWorker01(srv01, srv02, srv03)
+	regularWrk1.DoSomething()
 
+	regularWrk2, _ := createWorker02(srv02, srv03)
+	regularWrk2.DoSomething()
+
+	regularWrk3, _ := createWorker03(srv01, srv02)
+	regularWrk3.DoSomething()
+}
+
+func initializeWorkersWithDependencyInjection() {
 	// injecting parameters way:
 	// it firstly register each available parameter to be used later with their respective interface names. Then it
 	// calls the function that will be injected with the registered parameters as many times as it is needed without
 	// the need of passing each paramter separately again.
+
 	inj := lib.Injector{
 		AvailableParamebers: make(map[string]any),
 	}
